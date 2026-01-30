ef1fda69655119808c83361233b0426446d86283
Robert Alexandre <robert.alexandrea7@gmail.com>
2026-01-30 01:28:33 -0300

    Criados DTO para interpretar os dados que navegam para dentro e para fora da API

--- src/main/java/med/vol/api/controller/Medico.java
+++ src/main/java/med/vol/api/controller/Medico.java
@@ -5,12 +5,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import med.vol.api.medico.DadosCadastroMedico;
+
 @RestController
 @RequestMapping("/medicos") // Mapeia as requisições para /medicos
 public class Medico {
 
     @PostMapping
-    public void cadastraMedico(@RequestBody String json) {
+    public void cadastraMedico(@RequestBody DadosCadastroMedico dados) {
+        System.out.println(dados);
         return;
     }
 }
